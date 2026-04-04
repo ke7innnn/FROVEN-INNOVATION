aff623c52e71503acb0e8cefafda7bc4b167e8b7
Kevin <pinnaclestudios4u@gmail.com>
2026-04-04 13:31:44 +0530

    Fix: Resolve 'dynamic' variable name collision preventing Vercel build

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
-import dynamic from 'next/dynamic';
+import nextDynamic from 'next/dynamic';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 
 // Dynamically import everything below the fold to drastically reduce initial JS bundle size
-const Stats = dynamic(() => import('../components/Stats'));
-const MeetThePro = dynamic(() => import('../components/MeetThePro'));
-const CoolCollections = dynamic(() => import('../components/CoolCollections'));
-const CoolingSolutions = dynamic(() => import('../components/CoolingSolutions'));
-const ContactForm = dynamic(() => import('../components/ContactForm'));
-const Footer = dynamic(() => import('../components/Footer'));
+const Stats = nextDynamic(() => import('../components/Stats'));
+const MeetThePro = nextDynamic(() => import('../components/MeetThePro'));
+const CoolCollections = nextDynamic(() => import('../components/CoolCollections'));
+const CoolingSolutions = nextDynamic(() => import('../components/CoolingSolutions'));
+const ContactForm = nextDynamic(() => import('../components/ContactForm'));
+const Footer = nextDynamic(() => import('../components/Footer'));
 
 export const dynamic = 'force-static';
 
